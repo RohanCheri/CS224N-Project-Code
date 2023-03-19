e57c969a5e02806760f3661483e3f9b13fac6dd6
RohanCheri <rohankc03@gmail.com>
2023-03-18 21:11:24 -0700

    Revert tokenizer change

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -343,7 +343,7 @@ def load_and_cache_examples(args, task, tokenizer, evaluate=False, eval_split="t
                 sep_token_extra=bool(args.model_type in ['roberta']),
                 cls_token_segment_id=2 if args.model_type in ['xlnet'] else 0,
                 pad_on_left=bool(args.model_type in ['xlnet']),                 # pad on the left for xlnet
-                pad_token=tokenizer.pad_token,
+                pad_token=tokenizer.convert_tokens_to_ids([tokenizer.pad_token])[0],
                 pad_token_segment_id=4 if args.model_type in ['xlnet'] else 0
             )
         else:
